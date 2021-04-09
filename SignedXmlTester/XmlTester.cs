96c5d707dcb10f3c4569471d7eaecccb87b1d17c
Arve Hansen <arveh4@outlook.com>
2021-04-09 21:12:44 +0200

    Added XmlDocumentExtensions for signing Test that we have X509Certificate node after signing

--- SignedXmlTester/XmlTester.cs
+++ SignedXmlTester/XmlTester.cs
@@ -1,20 +1,18 @@
-using System.Security.Cryptography.Xml;
-using System.Xml;
 using FluentAssertions;
 using SignedXmlValidation;
 using SignedXmlValidation.CertStuff;
 using SignedXmlValidation.XmlStuff;
+using System.Xml;
 using Xunit;
 
 namespace SignedXmlTester
 {
     public class XmlTester
     {
-        private XmlDocument _doc;
-
-        public XmlTester()
+        [Fact]
+        public void TestXmlXCreator()
         {
-            _doc = XmlCreator.CreateXml(
+            var doc = XmlCreator.CreateXml(
                 Constants.Saml.AuthResponseId,
                 "assertion-id_2021-04-09T055047.8132195Z",
                 "id486364ad7bf040b0a3b6f35cc39c1ceb",
@@ -22,25 +20,30 @@ namespace SignedXmlTester
                 "gal.gadot@unit4.com",
                 "https://localhost:44300",
                 "https://localhost:44300/identity/AuthServices/Acs");
-        }
-
-        [Fact]
-        public void TestXmlXCreator()
-        {
-            _doc.OuterXml.Should().StartWith("<samlp:Response");
-
-            // "saml:Assertion"
-            var elementName = $"{Constants.XmlNSName.Saml}:{Constants.XmlElementNames.Assertion}";
+            doc.OuterXml.Should().StartWith("<samlp:Response");
         }
 
         [Fact]
         public void TestSigning()
         {
+            var elementId = "assertion-id_2021-04-09T055047.8132195Z";
+            var doc = XmlCreator.CreateXml(
+                Constants.Saml.AuthResponseId,
+                elementId,
+                "id486364ad7bf040b0a3b6f35cc39c1ceb",
+                "localhost:44388",
+                "gal.gadot@unit4.com",
+                "https://localhost:44300",
+                "https://localhost:44300/identity/AuthServices/Acs");
             // "saml:Assertion"
             var elementName = $"{Constants.XmlNSName.Saml}:{Constants.XmlElementNames.Assertion}";
-
             var x509 = CertCreator.GenerateX509Cert2();
-            var signedXml = new SignedXml(_doc) {SigningKey = x509.PrivateKey};
+            doc.Sign(x509, elementId, elementName);
+
+            var nsMgr = new XmlNamespaceManager(doc.NameTable);
+            nsMgr.AddNamespace("s", "http://www.w3.org/2000/09/xmldsig#");
+            var node = doc.SelectSingleNode("//s:X509Data", nsMgr);
+            node?.FirstChild?.Name.Should().Be("X509Certificate");
         }
 
     }
---
+++ SignedXmlValidation/XmlStuff/XmlDocumentExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace SignedXmlValidation.XmlStuff
+{
+    public static class XmlDocumentExtensions
+    {
+        public static void Sign(this XmlDocument doc,
+            X509Certificate2 cert, string elementId, string elementName = null)
+        {
+            if (doc == null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+
+            var rsaKey = ((RSA)cert.PrivateKey);
+
+            var signedXml = new SignedXml(doc) { SigningKey = rsaKey };
+            signedXml.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigExcC14NTransformUrl;
+
+            var keyInfo = new KeyInfo();
+            var keyInfoData = new KeyInfoX509Data(cert);
+            keyInfo.AddClause(keyInfoData);
+            signedXml.KeyInfo = keyInfo;
+
+            var reference = new Reference("#" + elementId);
+            reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
+            reference.AddTransform(new XmlDsigExcC14NTransform());
+            signedXml.AddReference(reference);
+
+            signedXml.ComputeSignature();
+            var signatureElement = signedXml.GetXml();
+
+            var signatureParent = string.IsNullOrWhiteSpace(elementName)
+                ? doc.DocumentElement
+                : doc.GetElementsByTagName(elementName)[0];
+            signatureParent?.AppendChild(doc.ImportNode(signatureElement, true));
+        }
+    }
+}
