38d006e45a0ec5e15cfc4a90ddef9725ca2ad7d3
Arve Hansen <arveh4@outlook.com>
2021-04-09 08:08:35 +0200

    Renamed project and testing

---
+++ SignedXmlTester/XmlTester.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using TestSignedXml;
+using Xunit;
+
+namespace SignedXmlTester
+{
+    public class XmlTester
+    {
+        [Fact]
+        public void TestXmlXCreator()
+        {
+            var creator = new XmlCreator();
+            var doc = creator.CreateXml(
+                Constants.Saml.AuthResponseId,
+                "assertion-id_2021-04-09T055047.8132195Z",
+                "id486364ad7bf040b0a3b6f35cc39c1ceb",
+                "localhost:44388",
+                "gal.gadot@unit4.com",
+                "https://localhost:44300",
+                "https://localhost:44300/identity/AuthServices/Acs");
+
+            doc.OuterXml.Should().StartWith("<samlp:Response");
+
+            // "saml:Assertion"
+            var elementName = $"{Constants.XmlNSName.Saml}:{Constants.XmlElementNames.Assertion}";
+        }
+    }
+}
